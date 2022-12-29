e648f29ad402da76c415761c798b76ea4f3dee59
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-12-29 23:38:47 +0700

    add firebase

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbU5mguymxg7ET5Cn0Q-VB3YHI5xhArdI",
+  authDomain: "clone-d002c.firebaseapp.com",
+  projectId: "clone-d002c",
+  storageBucket: "clone-d002c.appspot.com",
+  messagingSenderId: "235542093724",
+  appId: "1:235542093724:web:09899325f2397609327259",
+};
+
