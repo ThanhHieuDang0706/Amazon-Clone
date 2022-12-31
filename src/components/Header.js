96b2d6fac1fac0fb1bccc08d4e546fd2dc8d0f46
Hieu Dang <hieu.dang.dth_is_me@hcmut.edu.vn>
2022-12-31 10:58:16 +0700

    done authen

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ import {
 import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data: session } = useSession();
   return (
     <header>
       {/* Top Nav */}
@@ -33,8 +34,8 @@ const Header = () => {
 
         {/* Right */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div onClick={signIn} className="link">
-            <p>Hello Hieu</p>
+          <div onClick={!session ? signIn : signOut} className="link">
+            <p>{session ? `Hello ${session?.user?.name}` : "Sign In"}</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
 
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+
     // ...add more providers here
   ],
 };
