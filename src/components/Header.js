58b464c777a3065bc179851ce07294d138238775
Hieu Dang <hieu.dang.dth_is_me@hcmut.edu.vn>
2022-12-31 11:53:42 +0700

    add item to basket

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,25 @@ import Image from "next/image";
 import {
   MagnifyingGlassIcon,
   ShoppingCartIcon,
-  Bars3Icon,
+  Bars3Icon
 } from "@heroicons/react/24/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { selectItems } from "../slices/basketSlice";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems);
+
   return (
     <header>
       {/* Top Nav */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -24,7 +31,8 @@ const Header = () => {
         </div>
 
         {/* Search */}
-        <div className="hidden sm:flex items-center h-10 rounded-md cursor-pointer flex-grow    bg-yellow-400 hover:bg-yellow-500">
+        <div
+          className="hidden sm:flex items-center h-10 rounded-md cursor-pointer flex-grow    bg-yellow-400 hover:bg-yellow-500">
           <input
             className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
             type="text"
@@ -44,9 +52,10 @@ const Header = () => {
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
 
-          <div className="relative link flex items-center">
-            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-              0
+          <div onClick={() => router.push("/checkout")} className="relative link flex items-center">
+            <span
+              className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              {items.length}
             </span>
 
             <ShoppingCartIcon className="h-10" />
